cff22e0abbaa3075e89cffd5fddb992e48ea5f25
Francisco Hodge <hello@franciscohodge.com>
2018-12-02 16:01:25 -0500

    emoji-theme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ import "./index.css";
 
 let keyboard = new Keyboard({
   onChange: input => onChange(input),
-  onKeyPress: button => onKeyPress(button)
+  onKeyPress: button => onKeyPress(button),
+  layout: {
+    default: [
+      "\uD83D\uDE00 \uD83D\uDE01 \uD83D\uDE02 \uD83D\uDE03 \uD83D\uDE04 \uD83D\uDE05",
+      "\uD83D\uDE06 \uD83D\uDE07 \uD83D\uDE09 \uD83D\uDE0A \uD83D\uDE0B \uD83D\uDE0C",
+      "\uD83D\uDE0D \uD83D\uDE0E \uD83D\uDE0F \uD83D\uDE10 \uD83D\uDE11 \uD83D\uDE12",
+      "{bksp}"
+    ]
+  },
+  display: {
+    "{enter}": "submit",
+    "{bksp}": "backspace",
+    "{lock}": "lock",
+    "{shift}": "shift",
+    "{tab}": "tab",
+    "{space}": " "
+  },
+  layoutName: "default",
+  theme: "hg-theme-default emoji-theme"
 });
 
 /**
