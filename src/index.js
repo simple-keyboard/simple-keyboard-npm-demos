117300bf029c58729701c84ba388029c8e49c92e
Francisco Hodge <hello@franciscohodge.com>
2018-12-23 19:06:56 -0500

    git push

--- src/index.js
+++ src/index.js
@@ -4,7 +4,40 @@ import "./index.css";
 
 let keyboard = new Keyboard({
   onChange: input => onChange(input),
-  onKeyPress: button => onKeyPress(button)
+  onKeyPress: button => onKeyPress(button),
+  mergeDisplay: true,
+  layoutName: "default",
+  layout: {
+    default: [
+      "q w e r t y u i o p",
+      "a s d f g h j k l",
+      "{shift} z x c v b n m {backspace}",
+      "{numbers} {space} {ent}"
+    ],
+    shift: [
+      "Q W E R T Y U I O P",
+      "A S D F G H J K L",
+      "{shift} Z X C V B N M {backspace}",
+      "{numbers} {space} {ent}"
+    ],
+    numbers: ["1 2 3", "4 5 6", "7 8 9", "{abc} 0 {backspace}"]
+  },
+  display: {
+    "{numbers}": "123",
+    "{ent}": "return",
+    "{escape}": "esc ⎋",
+    "{tab}": "tab ⇥",
+    "{backspace}": "⌫",
+    "{capslock}": "caps lock ⇪",
+    "{shift}": "⇧",
+    "{controlleft}": "ctrl ⌃",
+    "{controlright}": "ctrl ⌃",
+    "{altleft}": "alt ⌥",
+    "{altright}": "alt ⌥",
+    "{metaleft}": "cmd ⌘",
+    "{metaright}": "cmd ⌘",
+    "{abc}": "ABC"
+  }
 });
 
 /**
@@ -28,6 +61,7 @@ function onKeyPress(button) {
    * If you want to handle the shift and caps lock buttons
    */
   if (button === "{shift}" || button === "{lock}") handleShift();
+  if (button === "{numbers}" || button === "{abc}") handleNumbers();
 }
 
 function handleShift() {
@@ -36,5 +70,14 @@ function handleShift() {
 
   keyboard.setOptions({
     layoutName: shiftToggle
+  });
+}
+
+function handleNumbers() {
+  let currentLayout = keyboard.options.layoutName;
+  let numbersToggle = currentLayout !== "numbers" ? "numbers" : "default";
+
+  keyboard.setOptions({
+    layoutName: numbersToggle
   });
 }
