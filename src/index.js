de8cdca58f0d9b5127eb1a8821444963c8045a3f
Francisco Hodge <hello@franciscohodge.com>
2025-02-17 16:26:03 -0500

    Add responsive demo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Keyboard from "simple-keyboard";
 import "simple-keyboard/build/css/index.css";
 import "./index.css";
 
+let isMobileWidth;
+
 let keyboard = new Keyboard({
   onChange: input => onChange(input),
   onKeyPress: button => onKeyPress(button)
@@ -28,10 +30,8 @@ function onChange(input) {
 function onKeyPress(button) {
   console.log("Button pressed", button);
 
-  /**
-   * If you want to handle the shift and caps lock buttons
-   */
-  if (button === "{shift}" || button === "{lock}") handleShift();
+  if (button === "{shiftleft}" || button === "{shiftright}" || button === "{capslock}" || button === "{shift}" || button === "{lock}") handleShift();
+  if (button === "{numbers}" || button === "{abc}") handleNumbers();
 }
 
 function handleShift() {
@@ -41,4 +41,103 @@ function handleShift() {
   keyboard.setOptions({
     layoutName: shiftToggle
   });
+}
+
+function handleNumbers() {
+  let currentLayout = keyboard.options.layoutName;
+  let numbersToggle = currentLayout !== "numbers" ? "numbers" : "default";
+
+  keyboard.setOptions({
+    layoutName: numbersToggle
+  });
+}
+
+function handleShift() {
+  let currentLayout = keyboard.options.layoutName;
+  let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+  keyboard.setOptions({
+    layoutName: shiftToggle
+  });
+}
+
+window.addEventListener('resize', resizeHandler);
+window.addEventListener('orientationchange', resizeHandler);
+resizeHandler();
+
+function resizeHandler() {
+  if (window.innerWidth <= 850) {
+    if (!isMobileWidth) {
+      isMobileWidth = true;
+      keyboard.setOptions({
+        mergeDisplay: true,
+        layoutName: "default",
+        theme: "hg-theme-default hg-layout-default hg-mobile-theme",
+        layout: {
+          'default': [
+            'q w e r t y u i o p',
+            'a s d f g h j k l',
+            '{shiftleft} z x c v b n m {backspace}',
+            '{numbers} {space} {ent}'
+          ],
+          'shift': [
+            'Q W E R T Y U I O P',
+            'A S D F G H J K L',
+            '{shiftleft} Z X C V B N M {backspace}',
+            '{numbers} {space} {ent}'
+          ],
+          'numbers': [
+            "1 2 3",
+            "4 5 6",
+            "7 8 9",
+            "{abc} 0 {backspace}",
+          ]
+        },
+        display: {
+          "{numbers}": "123",
+          "{ent}": "return",
+          "{escape}": "esc ⎋",
+          "{tab}": "tab ⇥",
+          "{backspace}": "⌫",
+          "{capslock}": "caps lock ⇪",
+          "{shiftleft}": "⇧",
+          "{shiftright}": "shift ⇧",
+          "{controlleft}": "ctrl ⌃",
+          "{controlright}": "ctrl ⌃",
+          "{altleft}": "alt ⌥",
+          "{altright}": "alt ⌥",
+          "{metaleft}": "cmd ⌘",
+          "{metaright}": "cmd ⌘",
+          "{abc}": "ABC"
+        }
+      });
+    }
+  } else {
+    if (isMobileWidth) {
+      isMobileWidth = false;
+
+      keyboard.setOptions({
+        mergeDisplay: true,
+        layoutName: "default",
+        theme: "hg-theme-default hg-layout-default",
+        layout: {
+          'default': [
+            '` 1 2 3 4 5 6 7 8 9 0 - = {backspace}',
+            '{tab} q w e r t y u i o p [ ] \\',
+            '{capslock} a s d f g h j k l ; \' {enter}',
+            '{shiftleft} z x c v b n m , . / {shiftright}',
+            '.com @ {space}'
+          ],
+          'shift': [
+            '~ ! @ # $ % ^ & * ( ) _ + {backspace}',
+            '{tab} Q W E R T Y U I O P { } |',
+            '{capslock} A S D F G H J K L : " {enter}',
+            '{shiftleft} Z X C V B N M < > ? {shiftright}',
+            '.com @ {space}'
+          ]
+        },
+        display: {}
+      });
+    }
+  }
 }
